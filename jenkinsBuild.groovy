08f5b171dbe1b69ebb9d93fe2922d0a268908261
Farkhod <sadykovfarkhod@gmail.com>
2020-07-22 18:54:47 -0500

    Deleted / from contians

--- jenkinsBuild.groovy
+++ jenkinsBuild.groovy
@@ -41,11 +41,12 @@ def slavePodTemplate = """
 
     if (branch == "master") {
       environment = "prod"
-    } else if (branch.contains('dev-feature/')) {
+    } else if (branch.contains('dev-feature')) {
       environment = "dev"
-    } else if (branch.contains('qa-feature/')) {
+    } else if (branch.contains('qa-feature')) {
       environment = "qa"
     }
+    println("${environment}")
 
 
     podTemplate(name: k8slabel, label: k8slabel, yaml: slavePodTemplate, showRawYaml: false) {
